2123ea8a273d066f3a161ec00275c507a2079b87
Cernan Bernardo <cernanb@gmail.com>
2017-12-19 22:35:21 -0800

    Finish and style Avatar component

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Constants } from 'expo'
 import { StyleSheet, Text, View } from 'react-native'
 
 import Avatar from './components/Avatar'
@@ -17,7 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    marginTop: Constants.statusBarHeight
   }
 })
